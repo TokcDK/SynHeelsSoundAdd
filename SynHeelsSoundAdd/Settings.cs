082d50d7e4383f743e5c6c4fe139d5392d5dcd53
TokcDK <TokcDK@Gmail.com>
2022-08-03 18:13:08 +0500

    added settings and option to add heels sound only for clothing boots

---
+++ SynHeelsSoundAdd/Settings.cs
@@ -0,0 +1,12 @@
+﻿using Mutagen.Bethesda.Synthesis.Settings;
+
+namespace SynHeelsSoundAdd
+{
+    public class Settings
+    {
+        [SynthesisSettingName("Add only for clothing")]
+        [SynthesisTooltip("Enable if want heels sound to be added only for clothing hh boots and not for armored")]
+        [SynthesisDescription("Determines if need to check only clothing boots and skip armors")]
+        public bool IsAddForClothingOnly { get; set; } = false;
+    }
+}
